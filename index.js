cd675882647a3c94995d93c8c6c7035e740c2219
MelCaro <m.caro@numendo.com>
2022-12-01 18:56:09 +0100

    add dotenv

---
+++ index.js
@@ -0,0 +1,11 @@
+//Répertorier les pièces jointes: shttps://learn.microsoft.com/fr-fr/graph/api/message-list-attachments?view=graph-rest-1.0&tabs=http
+//https://github.com/microsoftgraph/msgraph-sdk-javascript
+//https://learn.microsoft.com/fr-fr/graph/api/resources/attachment?view=graph-rest-1.0
+
+//https://developers.facebook.com/docs/whatsapp/cloud-api/get-started
+
+//notif discord ?
+
+import dotenv from "dotenv";
+
+dotenv.config();
